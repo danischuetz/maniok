106ed5ea9d27c5650a22a966af5b02ca6ec35c2c
Daniel Schütz <daniel@danielschuetz.dev>
2026-03-17 15:25:19 +0100

    Feature | View the first diagram from workspace.json | Part 1

--- packages/c4ke-lib/src/index.ts
+++ packages/c4ke-lib/src/index.ts
@@ -1,1 +1,8 @@
-export type { DiagramModel } from "./model/DiagramModel.js";
+export type { DiagramModel } from "./model/diagrammodel.js";
+export type { Element } from "./model/element.js";
+export type { ElementMetaData } from "./model/elementmetadata.js";
+export type { ElementType } from "./model/elementtype.js";
+export type { Relationship } from "./model/relationship.js";
+
+export { default as Diagram } from "./components/Diagram.svelte";
+export { default as DiagramProvider } from "./components/DiagramProvider.svelte";
