8825a7e29c9647e8c4a50fb8bf50c6770d46c334
Daniel Schütz <daniel@danielschuetz.dev>
2026-03-17 17:24:41 +0100

    Feature | View the first diagram from workspace.json | Part 4

--- packages/c4ke-lib/src/service/workspaceparser.ts
+++ packages/c4ke-lib/src/service/workspaceparser.ts
@@ -1,10 +1,8 @@
 import { SzrWorkspace, workspaceSchema } from "../model/szr/szrworkspace.js"
 
-class WorkspaceParser {
-    parse(workspaceJsonFileContents: string): SzrWorkspace {
+export class WorkspaceParser {
+    static parse(workspaceJsonFileContents: string): SzrWorkspace {
         const workspaceJson = JSON.parse(workspaceJsonFileContents)
         return workspaceSchema.parse(workspaceJson)
     }
 }
-
-export const workspaceParser = new WorkspaceParser()
