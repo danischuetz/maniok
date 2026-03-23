c27bec342674db8a2bb4bff0f48e387123929080
Daniel Schütz <dev@danielschuetz.dev>
2026-03-23 13:03:55 +0100

    Added group titles

--- packages/c4ke-lib/src/components/internal/diagram/group.svelte
+++ packages/c4ke-lib/src/components/internal/diagram/group.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
     import { type NodeProps } from '@xyflow/svelte'
+    import type { ElementMetaData } from '../../../model/diagram/elementmetadata'
 
-    let { width, height }: NodeProps = $props()
+    let { data, width, height }: NodeProps = $props()
 
-    // let metaData: NodeMetaData = $derived(data.metaData as NodeMetaData)
+    let metaData: ElementMetaData = $derived(data.metaData as ElementMetaData)
 </script>
 
-<div
-    class="absolute flex flex-col justify-end items-start"
-    style="width: {width}px; height: {height}px;"
-    role="group"
-></div>
+<div class="absolute inset-0 flex flex-col justify-start items-start" role="group">
+    {#if metaData.type && metaData.title}
+        <p class="px-1 group-label">{metaData.type}: {metaData.title}</p>
+    {/if}
+</div>
