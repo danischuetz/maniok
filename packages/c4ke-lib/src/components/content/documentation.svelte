de32bc888a2409727a0c7bb9334677d8a3754456
Daniel Schütz <dev@danielschuetz.dev>
2026-03-24 17:08:25 +0100

    We can render something

---
+++ packages/c4ke-lib/src/components/content/documentation.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { Mode } from '../../model/navigation/mode'
+    import ModeWrapper from '../internal/mode/modewrapper.svelte'
+
+    interface Props {
+        class?: string
+        html: string
+    }
+
+    let { class: className, html }: Props = $props()
+</script>
+
+<ModeWrapper mode={Mode.Documentation}>
+    <article class="markdown-body w-full h-full {className}">
+        {@html html}
+    </article>
+</ModeWrapper>
