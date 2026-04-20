b83ad2650f3871119cc75dbc52e3b3dcf31a88ca
Daniel Schütz <daniel@danielschuetz.dev>
2026-04-20 12:37:30 +0200

    refactor: Separate docker file for editor configuration

--- scripts/editor-entrypoint.sh
+++ scripts/editor-entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
 set -e
+
+# Check WORKSPACE_PATH
+if [[ -z "$WORKSPACE_PATH" || ! -d "$WORKSPACE_PATH" || ! -f "$WORKSPACE_PATH/workspace.dsl" ]]; then
+  echo "[ERROR] Directory /user/workspace must be mapped to a local directory containing a structurizr workspace." >&2
+  echo "Usage:"
+  echo "  docker run -t --rm -p 8080:8080 -v <path/to/workspace>:/usr/workspace  maniokeditor:latest" >&2
+  echo "Make sure <path/to/workspace> contains a workspace.dsl file." >&2
+  exit 1
+fi
 
 
 
