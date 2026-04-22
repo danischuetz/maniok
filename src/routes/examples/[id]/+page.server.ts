4c5efccf35d5e6657ac2c88f5c4b1a60a495a606
Daniel Schütz <daniel@danielschuetz.dev>
2026-04-22 15:35:04 +0200

    fix: Examples must be copied to the webapp image

--- src/routes/examples/[id]/+page.server.ts
+++ src/routes/examples/[id]/+page.server.ts
@@ -2,13 +2,16 @@ import type { PageServerLoad } from './$types'
 import { error } from '@sveltejs/kit'
 import { promises as fs } from 'fs'
 import path from 'path'
+import { env } from '$env/dynamic/private'
+
+const examplesPath = env.EXAMPLES_PATH ?? path.join(process.cwd(), 'examples', 'generated')
 
 async function loadFromLocalFile(filePath: string): Promise<string> {
     try {
         return await fs.readFile(filePath, 'utf-8')
     } catch (e) {
         if ((e as NodeJS.ErrnoException).code === 'ENOENT') {
-            error(500, `Workspace file not found at "${filePath}"`)
+            error(500, `Workspace file not found at "${filePath}".`)
         }
         throw error(500, `Error loading workspace file at "${filePath}": ${(e as Error).message}`)
     }
@@ -16,7 +19,7 @@ async function loadFromLocalFile(filePath: string): Promise<string> {
 
 export const load: PageServerLoad = async ({ params }) => {
     const exampleId: string = params.id
-    const examplePath: string = path.join('examples', 'generated', exampleId, 'workspace.json')
+    const examplePath: string = path.join(examplesPath, exampleId, 'workspace.json')
     const workspaceJson = await loadFromLocalFile(examplePath)
     return {
         repository: null,
